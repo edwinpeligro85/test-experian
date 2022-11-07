197848a2fe5cdf788a9c34e51946ea4923f682ba
Edwin Serna <edwinflow888@gmail.com>
2022-11-07 08:07:33 -0500

    :fire: Removing code/files: Se remueve la pagina About me

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,6 @@ const routes: Routes = [
       loadChildren: () =>
         import('./pages/account-overview/account-overview.module').then((m) => m.AccountOverviewModule),
     },
-    { path: 'about', loadChildren: () => import('./about/about.module').then((m) => m.AboutModule) },
   ]),
   // Fallback when no prior route is matched
   { path: '**', redirectTo: '', pathMatch: 'full' },
